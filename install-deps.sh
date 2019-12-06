1801405a6231636e21e680779b17a05b4fccf8b7
Yushan ZHANG <zhangysh1995@gmail.com>
2019-12-06 17:40:31 +0800

    python dependency

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install build-essential autoconf autoconf-archive libpqxx-dev libboost-regex-dev sqlite3 libsqlite3-dev fossil
+sudo apt-get install build-essential autoconf autoconf-archive python-pip python-dev libpqxx-dev libboost-regex-dev sqlite3 libsqlite3-dev fossil
 pip install executor --user
 pip install psycopg2 --user
 
